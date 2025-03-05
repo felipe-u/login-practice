a1045cac30b74561fe395952872bdc7002c07bd8
felipe-u <felipe_uv@outlook.com>
2025-03-04 19:32:24 -0500

    Se configura el servidor con un middleware y un soporte CORS

--- login-practice-back/app.js
+++ login-practice-back/app.js
@@ -1,11 +1,19 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
+// CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  next();
 });
 
 app.listen(PORT, () => {
