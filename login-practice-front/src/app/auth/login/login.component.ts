08dc3d76d6fd0c4965bd17ebb4b6eac34ed34efd
felipe-u <felipe_uv@outlook.com>
2025-03-04 21:52:26 -0500

    Implementamos ReactiveFormsMoudule

--- login-practice-front/src/app/auth/login/login.component.ts
+++ login-practice-front/src/app/auth/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required]
+    })
+  })
 
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
