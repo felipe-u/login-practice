40f78d2d801e66abb00daa1fe4ae6317390cff2f
felipe-u <felipe_uv@outlook.com>
2025-03-04 20:56:18 -0500

    Se añaden rutas de autenticación

--- login-practice-front/src/app/app.routes.ts
+++ login-practice-front/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from "@angular/router";
+import { routes as AuthRoutes} from "./auth/auth.routes";
 
-export const routes: Routes = []
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/auth',
+        pathMatch: 'full',
+    },
+    {
+        path: 'auth',
+        children: AuthRoutes
+    }
+]
