383b72edd33f4d5e504a80633bb5a3a7de357003
felipe-u <felipe_uv@outlook.com>
2025-03-07 22:01:55 -0500

    Se implementa la encriptación de contraseñas, se ajusta la respuesta a Frontend y se maneja una nueva excepción. Además de esto se hace una pequeña importación

--- login-practice-back/controllers/auth.js
+++ login-practice-back/controllers/auth.js
@@ -8,13 +8,18 @@ exports.register = (req, res, next) => {
   // Se recuperan los datos del front
   const name = req.body.name;
   const email = req.body.email;
-  const password = req.body.password;
+  const password = bcrypt.hashSync(req.body.password);
   const newUser = new User({ name: name, email: email, password: password });
 
   // Función mongoose
   newUser
     .save()
     .then(() => {
+      // dataUser solo almacena información no sensible
+      const dataUser = {
+        name: newUser.name,
+        email: newUser.email,
+      };
       const expiresIn = 24 * 60 * 60;
       const accessToken = jwt.sign({ id: newUser._id }, SECRET_KEY, {
         expiresIn: expiresIn,
@@ -23,15 +28,24 @@ exports.register = (req, res, next) => {
         success: true,
         message: "User registered successfully!",
         token: accessToken,
-        user: newUser,
+        expiresIn: expiresIn,
+        user: dataUser,
       });
     })
     .catch((err) => {
-      res.status(500).json({
-        success: false,
-        message: "Error registering user",
-        error: err.message,
-      });
+      if (err.code === 11000) {
+        res.status(409).json({
+          success: false,
+          message: "User already registered",
+          error: err.message,
+        });
+      } else {
+        res.status(500).json({
+          success: false,
+          message: "Error registering user",
+          error: err.message,
+        });
+      }
     });
 };
 
@@ -50,6 +64,10 @@ exports.login = (req, res, next) => {
         if (!isMatch) {
           return res.status(401).json({ message: "Invalid password" });
         }
+        const dataUser = {
+          name: user.name,
+          email: user.email,
+        };
         const expiresIn = 24 * 60 * 60;
         const accessToken = jwt.sign({ id: user._id }, SECRET_KEY, {
           expiresIn: expiresIn,
@@ -58,7 +76,8 @@ exports.login = (req, res, next) => {
           success: true,
           message: "User logged in successfully!",
           token: accessToken,
-          user: user,
+          expiresIn: expiresIn,
+          user: dataUser,
         });
       });
     })
--- login-practice-back/routes/auth.js
+++ login-practice-back/routes/auth.js
@@ -7,3 +7,5 @@ const authController = require('../controllers/auth');
 router.post("/register", authController.register);
 
 router.post("/login", authController.login);
+
+module.exports = router;
